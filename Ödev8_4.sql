ce106887790b8422fa407fe404725262ce5881c7
ozlemnurnazlioglu <119874721+ozlemnurazlioglu@users.noreply.github.com>
2025-10-17 14:06:59 +0300

    Add files via upload

---
+++ Ödev8_4.sql
@@ -0,0 +1,15 @@
+DELETE FROM employee
+WHERE id = 3;
+
+DELETE FROM employee
+WHERE name = 'John Doe';
+
+DELETE FROM employee
+WHERE birthday = '1990-12-12';
+
+DELETE FROM employee
+WHERE email = 'alice@example.com';
+
+DELETE FROM employee
+WHERE id = 4;
+
